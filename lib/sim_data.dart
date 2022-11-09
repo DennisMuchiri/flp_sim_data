5384ba6df15edf2510324015dd4eb308570a19a6
Dennis Muchiri <Dennis@Denniss-MacBook-Pro.local>
2022-11-09 09:32:59 +0300

    Initial commit

---
+++ lib/sim_data.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import './sim_model.dart';
+
+export './sim_model.dart';
+
+class SimDataPlugin {
+  static const String channelName = "com.vincentkammerer.sim_data/channel_name";
+  static const MethodChannel _channel = MethodChannel(channelName);
+  static Future<SimData> getSimData() async {
+    try {
+      dynamic simData = await _channel.invokeMethod('getSimData');
+      var data = json.decode(simData);
+      SimData simCards = SimData.fromJson(data);
+      return simCards;
+    } on PlatformException catch (e) {
+      debugPrint('SimDataPlugin failed to retrieve data $e');
+      rethrow;
+    }
+  }
+}
